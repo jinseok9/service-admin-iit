5bcdf58ced4298882c7f673b3b77f016bc238379
jinseok <jinseok@jobis.co>
2024-12-11 06:28:04 +0900

    feat: shared 맞게 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    federation({
+      name: "service-admin-iit",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./IIT": "./src/IIT.tsx",
+      },
+      shared: ["react", "react-dom"],
+    }),
+  ],
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 });
